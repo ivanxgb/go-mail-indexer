0d875312f79a49de00b4781d5363fe6cdc73f444
ivanxgb <ivanxgb@gmail.com>
2023-05-05 20:20:12 -0500

    chore: `model.env.go` created as template to load env variables. Needed to upload files to zinc

---
+++ indexer/env_loader/model.env.go
@@ -0,0 +1,44 @@
+package env_loader
+
+/*
+   To load the environment variables, uncomment the following lines and replace the values with the ones provided by Zinc.
+*/
+
+/*
+const (
+	API      = "https://api.zinc.dev/api"
+	ENDPOINT = "/COLLECTION_NAME/_json"
+
+	ORG_NAME = "your_org"
+	USERNAME = "your@mail.com"
+	PASSWORD = "YOUR_PASSWORD"
+)
+*/
+
+/*
+func ExportEnv() {
+	os.Setenv("API", API)
+	os.Setenv("ORG_NAME", ORG_NAME)
+	os.Setenv("ENDPOINT", ENDPOINT)
+	os.Setenv("USERNAME", USERNAME)
+	os.Setenv("PASSWORD", PASSWORD)
+}
+*/
+
+/*
+func GetUsername() string {
+	return os.Getenv("USERNAME")
+}
+*/
+
+/*
+func GetPassword() string {
+	return os.Getenv("PASSWORD")
+}
+*/
+
+/*
+func GetFullEndpoint() string {
+  return os.Getenv("API") + os.Getenv("ORG_NAME") + os.Getenv("ENDPOINT")
+}
+*/
