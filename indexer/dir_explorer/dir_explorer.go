95636006f56b91e3daf8000c5103784d4e3bb267
ivanxgb <ivanxgb@gmail.com>
2023-05-09 16:56:49 -0500

    ref: in `dir_explorer.go` removed `readFile` function. Added doc for `GetFilesPath`

--- indexer/dir_explorer/dir_explorer.go
+++ indexer/dir_explorer/dir_explorer.go
@@ -1,8 +1,6 @@
 package dir_explorer
 
 import (
-	"fmt"
-	"indexer/utils"
 	"os"
 	"path/filepath"
 )
@@ -11,36 +9,27 @@ const (
 	extensionFile = "."
 )
 
-func GetFilesInDirectory(dirPath string) {
-	var filesCount = 0
+// GetFilesPath receives a directory path and returns a slice of strings with
+// the path of all the files inside the directory.
+func GetFilesPath(dirPath string) ([]string, error) {
+	var filesPath []string
 
 	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		// skip directories and invalid files
-		if info.IsDir() || filepath.Ext(path) != extensionFile {
-			return nil
+		// If the path is not a directory or the file extension is valid, is added to the slice.
+		if !info.IsDir() || filepath.Ext(path) == extensionFile {
+			filesPath = append(filesPath, path)
 		}
 
-		filesCount++
-		fmt.Println("Files found: ", filesCount)
-		fmt.Println(readFile(path))
 		return nil
 	})
 
 	if err != nil {
-		utils.ErrorPrinter("There was an error reading the directory")
-	}
-}
-
-func readFile(filepath string) (string, error) {
-	file, err := os.ReadFile(filepath)
-	if err != nil {
-		fmt.Println("There was an error opening the file: " + filepath)
-		return "", err
+		return nil, err
 	}
 
-	return string(file), nil
+	return filesPath, nil
 }
