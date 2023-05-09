e3fedc3496c927886e212819465adb98aeef8870
ivanxgb <ivanxgb@gmail.com>
2023-05-09 17:02:12 -0500

    ref: in `utils.go` - `fileReader` moved from `dir_explorer.go`, refactored to be a public function.

--- indexer/utils/utils.go
+++ indexer/utils/utils.go
@@ -9,3 +9,14 @@ func ErrorPrinter(err string) {
 	fmt.Println("error: " + err)
 	os.Exit(1)
 }
+
+// FileReader receives a filepath and returns the file content as a string
+func FileReader(filepath string) (string, error) {
+	file, err := os.ReadFile(filepath)
+	if err != nil {
+		fmt.Println("There was an error opening the file: " + filepath)
+		return "", err
+	}
+
+	return string(file), nil
+}
