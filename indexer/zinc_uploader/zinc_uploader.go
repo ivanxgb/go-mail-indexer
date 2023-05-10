ef828e4ce39529112aedcbb0aa316450e135933e
ivanxgb <ivanxgb@gmail.com>
2023-05-09 23:05:07 -0500

    ref!: in `zinc_uploader.go` refactored package to launch go routines asynchronously to optimize the uploader

--- indexer/zinc_uploader/zinc_uploader.go
+++ indexer/zinc_uploader/zinc_uploader.go
@@ -7,39 +7,65 @@ import (
 	el "indexer/env_loader"
 	"indexer/model"
 	"net/http"
+	"sync"
 )
 
 const (
 	EmailBatchSize = 1000
+	GoRoutines     = 5
 )
 
 // SendFilesToServer receives a slice of strings that represent the path of the emails to be sent to the server
 func SendFilesToServer(filesPath []string) {
-	fmt.Println("Total files found: ", len(filesPath))
-	var emailJsonArray []model.Email
+	var wg sync.WaitGroup
+	totalFiles := len(filesPath)
+	fmt.Println("Total files to be processed: ", totalFiles)
 
-	var mailConverted = 0
-	var mailsWithError = 0
+	// divide the files in slices to be processed by the goroutines
+	filesSlicesSize := totalFiles / GoRoutines
+	indexSliceStart := 0
+	for i := 0; i < GoRoutines; i++ {
+		wg.Add(1)
+		indexSliceEnd := indexSliceStart + filesSlicesSize
+
+		// handle the remainder
+		if i == GoRoutines-1 {
+			indexSliceEnd = totalFiles
+		}
+		filesPathSlice := filesPath[indexSliceStart:indexSliceEnd]
+		indexSliceStart = indexSliceEnd
+
+		go ProcessFiles(filesPathSlice, &wg)
+	}
+
+	wg.Wait()
+
+}
+
+func ProcessFiles(filesPath []string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var emails []model.Email
 
 	for _, filePath := range filesPath {
 		email, err := de.EmailConverter(filePath)
 		if err != nil {
-			mailsWithError++
 			continue
 		}
 
-		mailConverted++
-		emailJsonArray = append(emailJsonArray, email)
+		// append the valid emails to the emails slice
+		emails = append(emails, email)
 
-		if len(emailJsonArray) == EmailBatchSize {
-			PostEmails(emailJsonArray)
-			emailJsonArray = nil
+		// if the emails slice is full, send it to the server
+		if len(emails) == EmailBatchSize {
+			PostEmails(emails)
+			emails = nil
 		}
 	}
 
-	fmt.Println("Total mails sent: ", mailConverted)
-	fmt.Println("Total mails with error or incorrect format: ", mailsWithError)
-
+	// send the remaining emails to the server
+	if len(emails) > 0 {
+		PostEmails(emails)
+	}
 }
 
 // PostEmails receives a slice of model.Email and converts it to json to be sent to the server
