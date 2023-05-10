b675350f34ce551aba6e3ba5757ec1ee62a9a026
ivanxgb <ivanxgb@gmail.com>
2023-05-09 21:41:58 -0500

    ref!: in `email_parser.go`, refactored `parseEmail` function to parse them using the native `net/mail` package

--- indexer/dir_explorer/email_parser.go
+++ indexer/dir_explorer/email_parser.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"indexer/model"
 	"indexer/utils"
-	"regexp"
+	"io"
+	"net/mail"
 	"strings"
 )
 
@@ -15,13 +16,16 @@ func EmailConverter(filePath string) (model.Email, error) {
 	content, err := utils.FileReader(filePath)
 
 	if err != nil {
-		fmt.Println("There was an error reading the file: " + filePath)
 		return model.Email{}, err
 	}
 
-	mail := parseMail(string(content))
+	emailParsed, err := parseMail(string(content))
 
-	return mail, nil
+	if err != nil {
+		return model.Email{}, err
+	}
+
+	return emailParsed, nil
 }
 
 // EmailsToJSON receives a slice of model.Email and returns a slice of bytes that represents the emails in json format
@@ -36,57 +40,46 @@ func EmailsToJSON(emails []model.Email) ([]byte, error) {
 }
 
 // parseMail receives a string that represents the content of the email and returns a model.Email struct with the email data
-func parseMail(content string) model.Email {
-	mail := model.Email{}
+func parseMail(content string) (model.Email, error) {
 
-	// re is a regular expression that matches the header fields of the email
-	// it matches the key and the value of the field to be able to extract the headers
-	re := regexp.MustCompile(`^(.*?):\s*(.*)$`)
+	emailAsReader := strings.NewReader(content)
+	emailParsed, err := mail.ReadMessage(emailAsReader)
 
-	// headerLines is a slice of strings that represents the header of the email
-	headerLines := strings.Split(content, "\n\n")[0]
-
-	headerFields := strings.Split(headerLines, "\n")
-	for _, field := range headerFields {
-		match := re.FindStringSubmatch(field)
-		if len(match) == 3 {
-			key := match[1]
-			value := match[2]
-
-			switch strings.ToLower(key) {
-			case "message-id":
-				mail.MessageID = value
-			case "date":
-				mail.Date = value
-			case "from":
-				mail.From = value
-			case "to":
-				mail.To = strings.Split(value, ",")
-			case "subject":
-				mail.Subject = value
-			case "cc":
-				mail.CC = strings.Split(value, ",")
-			case "bcc":
-				mail.BCC = strings.Split(value, ",")
-			case "x-from":
-				mail.XFrom = value
-			case "x-to":
-				mail.XTo = strings.Split(value, ",")
-			case "x-cc":
-				mail.XCC = strings.Split(value, ",")
-			case "x-bcc":
-				mail.XBCC = strings.Split(value, ",")
-			case "x-folder":
-				mail.XFolder = value
-			case "x-filename":
-				mail.XFileName = value
-			}
-		}
+	if err != nil {
+		return model.Email{}, err
 	}
 
-	// Extracting content
-	contentLines := strings.Split(content, "\n")[1:]
-	mail.Content = strings.Join(contentLines, "\n")
+	newEmail := model.Email{
+		MessageID: emailParsed.Header.Get("Message-Id"),
+		Date:      emailParsed.Header.Get("Date"),
+		From:      emailParsed.Header.Get("From"),
+		To:        parseHeaderList(emailParsed.Header.Get("To")),
+		Subject:   emailParsed.Header.Get("Subject"),
+		CC:        parseHeaderList(emailParsed.Header.Get("Cc")),
+		BCC:       parseHeaderList(emailParsed.Header.Get("Bcc")),
+		XFrom:     emailParsed.Header.Get("X-From"),
+		XTo:       parseHeaderList(emailParsed.Header.Get("X-To")),
+		XCC:       parseHeaderList(emailParsed.Header.Get("X-Cc")),
+		XBCC:      parseHeaderList(emailParsed.Header.Get("X-Bcc")),
+		XFolder:   emailParsed.Header.Get("X-Folder"),
+		XFileName: emailParsed.Header.Get("X-Filename"),
+	}
 
-	return mail
+	// Body from Reader to String
+	emailContent, err := io.ReadAll(emailParsed.Body)
+	if err == nil {
+		newEmail.Content = string(emailContent)
+	}
+	return newEmail, nil
 }
+
+func parseHeaderList(header string) []string {
+	// check if header is empty
+	if header == "" {
+		return nil
+	}
+
+	return strings.Split(header, ", ")
+}
+
+//Converting email: /Users/ivanxgb/Desktop/mails/maildir/baughman-d/calendar/19.
