1efa86525fe090d384ea6bde17b4bd82b10e1419
ivanxgb <ivanxgb@gmail.com>
2023-05-12 13:50:23 -0500

    ref!: in `routes.go` added method to receive a router and set the routes

--- mailer-backend/internal/app/api/routes.go
+++ mailer-backend/internal/app/api/routes.go
@@ -1,5 +1,14 @@
 package api
 
-const (
-	Search = "/search"
+import (
+	"github.com/go-chi/chi/v5"
+	"mailer-backend/internal/app/handler"
 )
+
+const (
+	searchRoute = "/search"
+)
+
+func setRoutes(router *chi.Mux) {
+	router.Post(searchRoute, handler.SearchHandler)
+}
