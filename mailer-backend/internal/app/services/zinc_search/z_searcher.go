e78157c9263da5cdf955e3e265bc511c7592bec3
ivanxgb <ivanxgb@gmail.com>
2023-05-12 15:16:52 -0500

    chore!: `z_searcher.go` created. It will handle the logic to send data to server

---
+++ mailer-backend/internal/app/services/zinc_search/z_searcher.go
@@ -0,0 +1,41 @@
+package zinc_search
+
+import (
+	"fmt"
+	"mailer-backend/internal/app/models"
+)
+
+func bodyBuilder(search string) models.ZSearchReq {
+	return models.ZSearchReq{
+		SearchType: matchAll,
+		Query: models.ZQuery{
+			Term:  search,
+			Field: allFields,
+		},
+	}
+}
+
+func SearchInMails(search string) ([]byte, error) {
+	body := bodyBuilder(search)
+	bodyAsJson, err := body.ToJson()
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := baseReq(bodyAsJson)
+
+	if err != nil {
+		fmt.Println("There was an error sending the request")
+		return nil, err
+	}
+
+	var zResp models.ZSearchResponse
+	err = zResp.FromJson(resp)
+
+	if err != nil {
+		fmt.Println("There was an error parsing the response")
+		return nil, err
+	}
+
+	return zResp.Hits.ToJson()
+}
