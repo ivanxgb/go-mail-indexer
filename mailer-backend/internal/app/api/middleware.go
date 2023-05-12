005d4b6fdcc7d6307b8f4fdd7d626267d56b0eae
ivanxgb <ivanxgb@gmail.com>
2023-05-12 13:51:00 -0500

    feat: `middleware.go` created to work setting entry point to set middlewares for api router

---
+++ mailer-backend/internal/app/api/middleware.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func setMiddlewares(router *chi.Mux) {
+	router.Use(
+		middleware.Logger,
+		middleware.AllowContentType("application/json"),
+		middleware.SetHeader("content-type", "application/json"),
+	)
+}
