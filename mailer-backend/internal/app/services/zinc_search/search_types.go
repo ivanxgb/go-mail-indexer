2335afe6dc47fe1c11841df983cc932d23bc5ffb
ivanxgb <ivanxgb@gmail.com>
2023-05-12 15:12:20 -0500

    chore: `search_types.go` file created to save as const the search types accepted by zinc search

---
+++ mailer-backend/internal/app/services/zinc_search/search_types.go
@@ -0,0 +1,11 @@
+package zinc_search
+
+const (
+	allFields    = "_all"
+	matchAll     = "matchall"
+	allDocuments = "alldocuments"
+	match        = "match"
+	matchPhrase  = "matchphrase"
+	term         = "term"
+	dateRange    = "daterange"
+)
