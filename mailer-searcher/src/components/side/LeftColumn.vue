63b344c40c4bd34d208840e7e1cda89be9b31c4b
ivanxgb <ivanxgb@gmail.com>
2023-05-13 17:15:31 -0500

    chore: `LeftColumn.vue` component created to vertical nav

---
+++ mailer-searcher/src/components/side/LeftColumn.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import Search from '@/components/side/Search.vue'
+import PreMail from '@/components/mail/PreMail.vue'
+</script>
+
+<template>
+  <div class="border-x-2 mt-6 w-1/3 xl:w-1/4 2xl:w-1/5">
+    <Search />
+    <PreMail
+      class="px-4"
+      date="Jul 25, 2023"
+      msg="Hey! How is everything?"
+      name="Stefanny Guzman"
+    />
+  </div>
+</template>
+
+<style scoped></style>
