4db6a25f7ca95e0c396acb1b9b5640917de70672
ivanxgb <ivanxgb@gmail.com>
2023-05-13 13:17:56 -0500

    chore: created component `PreMail` to display a preview of the mail

---
+++ mailer-searcher/src/components/mail/PreMail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="w-full pb-4 border-y border-gray-300 hover:bg-slate-100 hover:cursor-pointer">
+    <div class="flex items-center py-2">
+      <div class="flex items-center justify-center w-12 h-12 rounded-full shadow-lg">
+        <p class="text-gray-600 font-medium">{{ nameInitials }}</p>
+      </div>
+      <div class="w-full ml-2">
+        <div class="flex items-center justify-between">
+          <h3 class="text-sm line-clamp-1 text-gray-800">{{ name }}</h3>
+          <p class="text-xs text-gray-600">{{ date }}</p>
+        </div>
+        <div class="flex items-center">
+          <p class="mr-1 text-xs text-indigo-600">Team appreciation</p>
+        </div>
+      </div>
+    </div>
+    <p class="text-justify text-xs line-clamp-2 text-gray-600">
+      Greatest appreciation to you and your team for the outstanding job you did for us. The website
+      is just what we wanted, and we were thrilled with the speed your team exercised. We feel
+      privileged to have eLab communications.
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  msg: String,
+  name: String,
+  date: String
+})
+
+const nameInitials = computed(() => {
+  const words = props.name?.split(' ')
+  return `${words?.[0]?.[0].toUpperCase() ?? 'N'}${words?.[1]?.[0].toUpperCase() ?? 'N'}`
+})
+</script>
