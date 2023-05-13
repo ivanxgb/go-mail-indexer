5ceacee735df44c3e879eac976a0754391b3662c
ivanxgb <ivanxgb@gmail.com>
2023-05-12 19:16:21 -0500

    chore: component `Header.vue` created for `<header>`

---
+++ mailer-searcher/src/components/header/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav
+    class="flex items-center justify-between text-gray-800 mt-3 py-6 px-7 font-normal border-b border-gray-100 shadow-md rounded-b-md"
+  >
+    <h1 class="text-3xl">Van Mail</h1>
+    <div>
+      <p class="text-sm text-gray-600">Hi Ivan</p>
+      <h3 class="font-bold text-xl">Welcome Back</h3>
+    </div>
+  </nav>
+</template>
