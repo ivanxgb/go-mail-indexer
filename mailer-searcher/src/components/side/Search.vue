ee1bb4602375be739952b58e0802717387c41837
ivanxgb <ivanxgb@gmail.com>
2023-05-13 13:17:28 -0500

    chore: created component `Search` for the search bar

---
+++ mailer-searcher/src/components/side/Search.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="relative w-full flex justify-start items-center text-gray-800 border-t-2">
+    <input
+      class="w-full pl-16 py-3 rounded placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-600"
+      placeholder="Search"
+      type="text"
+    />
+    <img alt="search" class="absolute w-4 left-4" src="../../assets/magnifier.svg" />
+  </div>
+</template>
