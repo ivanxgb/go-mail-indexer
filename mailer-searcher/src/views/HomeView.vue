b8f0aca65c0ec544f915796688c1a3576dae60ef
ivanxgb <ivanxgb@gmail.com>
2023-05-13 17:15:43 -0500

    ref: importing components

--- mailer-searcher/src/views/HomeView.vue
+++ mailer-searcher/src/views/HomeView.vue
@@ -1,3 +1,11 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import LeftColumn from '@/components/side/LeftColumn.vue'
+import Mail from '@/components/mail/Mail.vue'
+</script>
 
-<template></template>
+<template>
+  <div class="flex">
+    <LeftColumn class="ml-1" />
+    <Mail class="flex-1" />
+  </div>
+</template>
