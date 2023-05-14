386ce2e93a9eaf02a1e5392e0b99eac31b04b356
ivanxgb <ivanxgb@gmail.com>
2023-05-14 17:07:56 -0500

    ref!: in `LeftColumn.vue`, getting mails from store and passing them to `PreMail` component

--- mailer-backend/ui/src/components/side/LeftColumn.vue
+++ mailer-backend/ui/src/components/side/LeftColumn.vue
@@ -1,17 +1,22 @@
 <script lang="ts" setup>
-import Search from '@/components/side/Search.vue'
-import PreMail from '@/components/mail/PreMail.vue'
+import Search from "@/components/side/Search.vue";
+import { useEmailStore } from "@/stores/email_store";
+import { onMounted } from "vue";
+import PreMail from "@/components/mail/PreMail.vue";
+import { storeToRefs } from "pinia";
+
+const mailStore = useEmailStore();
+const { mails } = storeToRefs(mailStore);
+
+onMounted(() => {
+  mailStore.fetchMails("ivan");
+});
 </script>
 
 <template>
   <div class="border-x-2 mt-6 w-1/3 xl:w-1/4 2xl:w-1/5">
     <Search />
-    <PreMail
-      class="px-4"
-      date="Jul 25, 2023"
-      msg="Hey! How is everything?"
-      name="Stefanny Guzman"
-    />
+    <PreMail v-for="mail in mails" :key="mail.id" :mailData="mail" />
   </div>
 </template>
 
