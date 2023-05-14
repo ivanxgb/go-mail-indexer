090532bd5e485e7c69c9def16764c1b0967e1664
ivanxgb <ivanxgb@gmail.com>
2023-05-14 16:54:21 -0500

    ref!: in `main.go` added functions to print server address as hyperlink

--- mailer-backend/cmd/main.go
+++ mailer-backend/cmd/main.go
@@ -7,6 +7,11 @@ import (
 	el "mailer-backend/env_loader"
 	"mailer-backend/internal/app/api"
 	"net/http"
+	"os/exec"
+)
+
+const (
+	zSearchPort = ":4080"
 )
 
 var (
@@ -18,12 +23,31 @@ func main() {
 	flag.Parse()
 	port := fmt.Sprintf(":%s", *port)
 
+	go startZSearcher()
+	printLink(zSearchPort, "ZSearch is running on")
 	router := api.GetRouter()
 
-	fmt.Println("Server running on port", port)
+	printLink(port, "Server is running on")
+
 	err := http.ListenAndServe(port, router)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func startZSearcher() {
+	cmd := exec.Command("sh", "./z_launcher.sh")
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func printLink(port string, cliText string) {
+	url := fmt.Sprintf("http://localhost%s", port)
+	displayText := fmt.Sprintf("%s %s", cliText, url)
+
+	link := fmt.Sprintf("\033]8;;%s\033\\%s\033]8;;\033\\", url, displayText)
+	fmt.Println(link)
 }
