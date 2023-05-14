04185fae2df5c6c194477771d693b38f403d171c
ivanxgb <ivanxgb@gmail.com>
2023-05-14 16:55:10 -0500

    ref: in `utils.ts` created method for debounce
    ref: in `.prettierrc.json` modified rules

--- mailer-backend/ui/src/utils/utils.ts
+++ mailer-backend/ui/src/utils/utils.ts
@@ -2,3 +2,11 @@ export const getInitialsName = (name?: string) => {
   const words = name?.split(' ')
   return `${words?.[0]?.[0].toUpperCase() ?? 'N'}${words?.[1]?.[0].toUpperCase() ?? 'N'}`
 }
+
+export const debounce = (func: Function, delay: number = 400) => {
+  let timeout: NodeJS.Timeout;
+  return (...args: any) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), delay);
+  };
+};
