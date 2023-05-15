d7b7f825195244fb0263299e89123c0a19496fd3
ivanxgb <ivanxgb@gmail.com>
2023-05-15 15:12:09 -0500

    chore: code rearranged

--- mailer-backend/ui/src/views/HomeView.vue
+++ mailer-backend/ui/src/views/HomeView.vue
@@ -1,12 +1,12 @@
+<script lang="ts" setup>
+import LeftColumn from "@/components/side/LeftColumn.vue";
+import Mail from "@/components/mail/Mail.vue";
+import { useEmailStore } from "@/stores/email_store";
+</script>
+
 <template>
   <div class="flex">
     <LeftColumn class="ml-1" />
     <Mail v-if="useEmailStore().mailSelected !== null" class="flex-1" />
   </div>
 </template>
-
-<script lang="ts" setup>
-import LeftColumn from "@/components/side/LeftColumn.vue";
-import Mail from "@/components/mail/Mail.vue";
-import { useEmailStore } from "@/stores/email_store";
-</script>
