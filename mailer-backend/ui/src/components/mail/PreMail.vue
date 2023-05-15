e3c0ae3407400ad2288f8b976117dcd9a09d6644
ivanxgb <ivanxgb@gmail.com>
2023-05-15 12:14:55 -0500

    ref!: in `PreMail` component, simplified prop definition. Added method to select mail on click. parsing date
    Reformatted using prettier

--- mailer-backend/ui/src/components/mail/PreMail.vue
+++ mailer-backend/ui/src/components/mail/PreMail.vue
@@ -1,6 +1,9 @@
 <template>
   <div
+    :class="isMailSelected ? 'bg-slate-100' : ''"
     class="w-full pb-4 border-y border-gray-300 hover:bg-slate-100 hover:cursor-pointer"
+    role="button"
+    @click="handleMailClick"
   >
     <div class="flex items-center py-2">
       <div
@@ -11,19 +14,19 @@
       <div class="w-full ml-2">
         <div class="flex items-center justify-between">
           <h3 class="text-sm line-clamp-1 text-gray-800">
-            {{ props.mailData.mail.from }}
+            {{ mail?.["x-from"] }}
           </h3>
-          <p class="text-xs text-gray-600">{{ props.mailData.mail.date }}</p>
+          <p class="text-xs text-gray-600">{{ dateParsed }}</p>
         </div>
         <div class="flex items-center">
           <p class="mr-1 text-xs text-indigo-600">
-            {{ props.mailData.mail.subject }}
+            {{ mail?.subject }}
           </p>
         </div>
       </div>
     </div>
     <p class="text-justify text-xs line-clamp-2 text-gray-600">
-      {{ props.mailData.mail.content }}
+      {{ mail?.content }}
     </p>
   </div>
 </template>
@@ -31,14 +34,18 @@
 <script lang="ts" setup>
 import { computed, defineProps } from "vue";
 import type { MailData } from "@/model/SearchResponseModel";
-import { getInitialsName } from "@/utils/utils";
+import { getInitialsName, parseDate } from "@/utils/utils";
+import { useEmailStore } from "@/stores/email_store";
 
-const props = defineProps({
-  mailData: {
-    type: Object as () => MailData,
-    required: true
-  }
-});
+const props = defineProps<{ mailData: MailData }>();
 
-const nameInitials = computed(() => getInitialsName(props.mailData?.mail.from));
+const store = useEmailStore();
+
+const { mailData } = props;
+const mail = computed(() => mailData?.mail);
+
+const handleMailClick = () => mailData && store.selectMail(mailData);
+const dateParsed = computed(() => parseDate(mail?.value?.date));
+const isMailSelected = computed(() => mailData?.id === store.mailSelected?.id);
+const nameInitials = computed(() => getInitialsName(mail?.value?.from));
 </script>
