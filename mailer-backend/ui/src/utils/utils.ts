e47e091b86942fc31238c829e0525fb04b2752f8
ivanxgb <ivanxgb@gmail.com>
2023-05-16 16:51:05 -0500

    ref!: added function to clean text by regex

--- mailer-backend/ui/src/utils/utils.ts
+++ mailer-backend/ui/src/utils/utils.ts
@@ -5,6 +5,13 @@ export const getInitialsName = (name?: string) => {
   }`;
 };
 
+export const cleanEnronFormat = (text?: string) => {
+  if (text === undefined) return "";
+
+  const identifierRegex = /<\/O=ENRON\/OU=NA\/CN=RECIPIENTS\/CN=[^>]+>/g;
+  return text.replace(identifierRegex, "");
+};
+
 export const parseDate = (date?: string) => {
   const parsedDate = new Date(date ?? "15/05/2023");
   return parsedDate.toLocaleDateString("en-US", {
