1838f381d914dba5bbf407be4e06ee9e6914a783
ivanxgb <ivanxgb@gmail.com>
2023-05-15 19:00:22 -0500

    ref!: commented profiler code

--- indexer/cmd/main.go
+++ indexer/cmd/main.go
@@ -1,25 +1,22 @@
 package main
 
 import (
-	"fmt"
 	ia "indexer/internal/app/init"
-	"os"
-	"runtime/pprof"
 )
 
 func main() {
-	profilerFile, err := os.Create("z_indexer.pprof")
-	if err != nil {
-		fmt.Println("There was an error creating the profiler", err)
-		return
-	}
-	defer profilerFile.Close()
-	err = pprof.StartCPUProfile(profilerFile)
-	if err != nil {
-		return
-	}
-
-	defer pprof.StopCPUProfile()
+	//profilerFile, err := os.Create("z_indexer.pprof")
+	//if err != nil {
+	//	fmt.Println("There was an error creating the profiler", err)
+	//	return
+	//}
+	//defer profilerFile.Close()
+	//err = pprof.StartCPUProfile(profilerFile)
+	//if err != nil {
+	//	return
+	//}
+	//
+	//defer pprof.StopCPUProfile()
 
 	ia.Init()
 }
