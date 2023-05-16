41194bc4a60d013549a77628eb670cad3f8f5c07
ivanxgb <ivanxgb@gmail.com>
2023-05-16 17:24:07 -0500

    chore: code cleaned

--- mailer-backend/ui/src/components/mail/PreMail.vue
+++ mailer-backend/ui/src/components/mail/PreMail.vue
@@ -1,3 +1,23 @@
+<script lang="ts" setup>
+import { computed, defineProps } from "vue";
+import type { MailData } from "@/model/SearchResponseModel";
+import { cleanEnronFormat, getInitialsName, parseDate } from "@/utils/utils";
+import { useEmailStore } from "@/stores/email_store";
+
+const props = defineProps<{ mailData: MailData }>();
+const { mailData } = props;
+const mail = computed(() => mailData?.mail);
+
+const store = useEmailStore();
+
+const handleMailClick = () => mailData && store.selectMail(mailData);
+const dateParsed = computed(() => parseDate(mail?.value?.date));
+const fromCleaned = computed(() => cleanEnronFormat(mail?.value?.from));
+const isMailSelected = computed(() => mailData?.id === store.mailSelected?.id);
+const nameInitials = computed(() => getInitialsName(fromCleaned.value));
+const xFromCleaned = computed(() => cleanEnronFormat(mail?.value?.["x-from"]));
+</script>
+
 <template>
   <div
     :class="isMailSelected ? 'bg-slate-100' : ''"
@@ -14,7 +34,7 @@
       <div class="w-full ml-2">
         <div class="flex items-center justify-between">
           <h3 class="text-sm line-clamp-1 text-gray-800">
-            {{ mail?.["x-from"] }}
+            {{ xFromCleaned }}
           </h3>
           <p class="text-xs text-gray-600">{{ dateParsed }}</p>
         </div>
@@ -30,22 +50,3 @@
     </p>
   </div>
 </template>
-
-<script lang="ts" setup>
-import { computed, defineProps } from "vue";
-import type { MailData } from "@/model/SearchResponseModel";
-import { getInitialsName, parseDate } from "@/utils/utils";
-import { useEmailStore } from "@/stores/email_store";
-
-const props = defineProps<{ mailData: MailData }>();
-
-const store = useEmailStore();
-
-const { mailData } = props;
-const mail = computed(() => mailData?.mail);
-
-const handleMailClick = () => mailData && store.selectMail(mailData);
-const dateParsed = computed(() => parseDate(mail?.value?.date));
-const isMailSelected = computed(() => mailData?.id === store.mailSelected?.id);
-const nameInitials = computed(() => getInitialsName(mail?.value?.from));
-</script>
