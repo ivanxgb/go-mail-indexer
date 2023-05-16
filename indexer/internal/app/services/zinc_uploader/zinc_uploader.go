5621a63cf65c8140ddf99552fced2c8e674a97bb
ivanxgb <ivanxgb@gmail.com>
2023-05-15 20:07:34 -0500

    ref!: `zinc_uploader` added function to receive batch of mails and sent it to server.

--- indexer/internal/app/services/zinc_uploader/zinc_uploader.go
+++ indexer/internal/app/services/zinc_uploader/zinc_uploader.go
@@ -4,105 +4,30 @@ import (
 	"bytes"
 	"fmt"
 	el "indexer/env_loader"
-	de "indexer/internal/app/dir_explorer"
 	"indexer/internal/app/model"
 	"net/http"
-	"sync"
 )
 
-const (
-	EmailBatchSize = 1000
-	GoRoutines     = 4
-)
-
-// ProcessFiles receives a slice of strings that represent the path of the emails to be sent to the server
-func ProcessFiles(filePaths *[]string) {
-	files := *filePaths
-	totalFiles := len(files)
-	filesSlicesSize := totalFiles / GoRoutines
-	var fileErrorCounter, errorPostCounter, indexSliceStart int
-	fmt.Println("Total files to be processed: ", totalFiles)
-
-	var wg sync.WaitGroup
-	wg.Add(GoRoutines)
-	filesErrorChan := make(chan int, GoRoutines)
-	failMailsUpload := make(chan int, GoRoutines)
-
-	for i := 0; i < GoRoutines; i++ {
-		indexSliceEnd := indexSliceStart + filesSlicesSize
-
-		// handle the remainder
-		if i == GoRoutines-1 {
-			indexSliceEnd = totalFiles
-		}
-
-		filesPathSlice := files[indexSliceStart:indexSliceEnd]
-		indexSliceStart = indexSliceEnd
-
-		go processFiles(filesPathSlice, &wg, filesErrorChan, failMailsUpload)
-	}
-
-	wg.Wait()
-	close(filesErrorChan)
-	close(failMailsUpload)
-
-	for err := range filesErrorChan {
-		fileErrorCounter += err
-	}
-
-	for err := range failMailsUpload {
-		errorPostCounter += err
-	}
-
-	fmt.Println("Total files with errors: ", fileErrorCounter)
-	fmt.Println("Total files don't uploaded: ", errorPostCounter)
-	fmt.Println("Total files uploaded: ", totalFiles-fileErrorCounter-errorPostCounter)
-}
-
-func processFiles(filePaths []string, wg *sync.WaitGroup, errorChan chan int, failMailsUpload chan int) {
-	defer wg.Done()
-	var errorFilesCounter, errorPostCounter int
-	var emails []model.Email
-
-	for _, filePath := range filePaths {
-		email, err := de.EmailConverter(filePath)
-		if err != nil {
-			errorFilesCounter++
-			continue
-		}
-
-		// append the valid emails to the emails slice
-		emails = append(emails, email)
-
-		// if the emails slice is full, send it to the server
-		if len(emails) == EmailBatchSize {
-			if !processEmails(&emails) {
-				errorPostCounter += EmailBatchSize
-			}
-			emails = nil
-		}
-	}
-
-	// send the remaining emails to the server
-	if len(emails) > 0 {
-		if !processEmails(&emails) {
-			errorPostCounter += len(emails)
-		}
-	}
-
-	failMailsUpload <- errorPostCounter
-	errorChan <- errorFilesCounter
-}
-
-// processEmails receives a slice of model.Email and converts it to json to be sent to the server
-func processEmails(emails *[]model.Email) bool {
-	jsonEmails, err := de.EmailsToBulkJson(emails)
+// UploadMails receives a slice of model.Email to be sent to the server
+func UploadMails(emails *[]model.Email, index string) bool {
+	jsonEmails, err := bodyBuilder(emails, index)
 	if err != nil {
 		fmt.Println("There was an error converting the emails to json")
 		return false
 	}
 
 	return postData(jsonEmails)
+}
+
+// bodyBuilder receives a slice of model.Email and returns a slice of bytes that represents the emails in
+// json format accepted by the server
+func bodyBuilder(emails *[]model.Email, index string) ([]byte, error) {
+	emailBulk := model.BulkV2{
+		Index:   index,
+		Records: *emails,
+	}
+
+	return emailBulk.ToJson()
 }
 
 // postData receives a slice of bytes that represents the data to be sent
